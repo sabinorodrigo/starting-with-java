b6a40fe8864e1d431e847afd3db2162b4ba227ce
Rodrigo Sabino Gomes <sr.rodrigosabino@gmail.com>
2023-07-11 10:41:47 -0300

    add file Sum.java

---
+++ java-code/Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+    public static void main(String[] args) {
+        int num1 = 130;
+        int num2 = 432;
+
+        int total = num1 + num2;
+
+        System.out.println(total);
+    }
+}
