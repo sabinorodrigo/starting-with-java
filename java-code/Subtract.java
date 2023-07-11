5d6e96c03783e18add353c451d2437fe491f7e75
Rodrigo Sabino Gomes <sr.rodrigosabino@gmail.com>
2023-07-11 13:36:30 -0300

    add new file, Subtract.java

---
+++ java-code/Subtract.java
@@ -0,0 +1,10 @@
+public class Subtract {
+    public static void main(String[] args) {
+        int num1 = 500;
+        int num2 = 250;
+
+        int total = num1 - num2;
+
+        System.out.printf("The subtract of %d and %d is %d.", num1, num2, total);
+    }
+}
